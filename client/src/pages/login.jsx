5a0637777574d68ad28740e6e83787a3b03bacee
Divyansh Tanwar <divyanshtanwar04@gmail.com>
2024-05-30 00:01:48 +0530

    login page front end created

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
 import React from "react";
-
+import { FcGoogle } from 'react-icons/fc';
 function login() {
-  return <div>login</div>;
+  return <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
+    <div className="flex justify-center items-center">
+     <Image src="/INDIA.gif" alt="whatsapp"  height={700} width={700} />
+    </div>
+    <div className="flex  gap-2 justify-center items-center">
+     <span className="text-3xl bg-clip-text text-transparent bg-[linear-gradient(to_right,#FFA500,#FFA500,#FFFFFF,#FFFFFF,#FFFFFF,#00FF00,#00FF00)]">Indi</span>
+     <span className="text-7xl bg-clip-text text-transparent bg-[linear-gradient(to_right,#FFA500,#FFA500,#FFFFFF,#FFFFFF,#FFFFFF,#00FF00,#00FF00)]">Chat</span>
+     <span className="text-lg pt-12 px-2 bg-clip-text text-transparent bg-[linear-gradient(to_right,#FFA500,#FFA500,#FFFFFF,#FFFFFF,#FFFFFF,#00FF00,#00FF00)]">Judiye Apno Se...</span>
+     </div>
+    <button className="flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg hover:border-sky-200 hover:shadow-[0_0_2px_#FFA500,inset_0_0_2px_#FFA500,0_0_5px_#FFA500,0_0_5px_#FFA500,0_0_10px_#FFA500] hover:transition duration-1000 ease-in-out">
+    <FcGoogle className="text-4xl"/>
+    <span className="text-white  text-2xl">Login With Google</span>
+    </button>
+  </div>;
 }
 
 export default login;
