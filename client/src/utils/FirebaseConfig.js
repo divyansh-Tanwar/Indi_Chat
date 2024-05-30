78706e092c79f4821a173dde63ee6dd5ced39745
Divyansh Tanwar <divyanshtanwar04@gmail.com>
2024-05-30 21:45:46 +0530

    authentication from google using firebase is done (handleLogin function in Login.jsx)

--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDzDAGlIwKxjeUOa7exlMi1rFP8Nm4JCsE",
-    authDomain: "whatsapp-clone-7005a.firebaseapp.com",
-    projectId: "whatsapp-clone-7005a",
-    storageBucket: "whatsapp-clone-7005a.appspot.com",
-    messagingSenderId: "29285982241",
-    appId: "1:29285982241:web:f80959c9c41081a121c360",
-    measurementId: "G-PZYRD06PZ0"
-  };
+  apiKey: "AIzaSyBeVbwkaDj-q398CEr2iD_jEtspLSKoq3k",
+  authDomain: "indichat-286d7.firebaseapp.com",
+  projectId: "indichat-286d7",
+  storageBucket: "indichat-286d7.appspot.com",
+  messagingSenderId: "886997959372",
+  appId: "1:886997959372:web:543f30c63f3a5c6b60f869",
+  measurementId: "G-70858F669V"
+};
 
   const app=initializeApp(firebaseConfig);
   export const firebaseAuth=getAuth(app);
